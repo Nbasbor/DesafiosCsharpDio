da0d943f9721ef1c909163916ecb858e62dd7649
Basbor <nanbasbor@gmail.com>
2021-10-11 15:19:52 -0300

    Adicionando Nível 3

---
+++ DesafiosDio/DesafioDioNivel3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDioNivel3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declaração das variáveis globais
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+            int pref = 0;
+           
+
+            do
+
+            {
+
+                pref = Convert.ToInt32(Console.ReadLine());
+
+                switch (pref)
+                {
+                    case 1:
+                        {
+                            alcool++;
+                            break;
+                        }
+                    case 2:
+                        {
+                            gasolina++;
+                            break;
+                        }
+                    case 3:
+                        {
+                            diesel++;
+                            break;
+                        }
+                    case 4:
+                        //Console.WriteLine("Fim");
+                        Console.WriteLine("MUITO OBRIGADO");
+
+                        break;
+                    default:
+                        // Console.WriteLine("teste");
+                        continue;
+                }
+
+
+
+
+            } while (pref != 4);
+            
+
+            Console.WriteLine("Alcool: {0}", alcool);
+            Console.WriteLine("Gasolina: {0}", gasolina);
+            Console.WriteLine("Diesel: {0}", diesel);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -110,3 +110,17 @@ Imprima uma linha, contendo a frase "Farei hoje!" caso seja possível fabricar o
 
 
 
+<h2 align="center">Nível 3</h2>
+
+<h3 align="center">Desafio 1 **Tipo de Combustível**</h3>
+
+<p align="center">
+<h4 align="center">Desafio Proposto</h4>
+Você recebeu desafio para determinar qual dos produtos é o preferêncial dos clientes de um posto de abastecimento de combustível. Para isso você deve escrever um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4.
+
+<h4 align="center">Entrada</h4>
+A entrada contém apenas valores inteiros e positivos.
+
+<h4 align="center">Saída</h4>
+Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme exemplo.
+</p>
